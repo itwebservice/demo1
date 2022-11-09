d0a1fedcfae0fa16a452c02a43ec7422b7844ab9
shubhampitweb <shubhampatil.itweb@gmail.com>
2022-11-09 09:28:16 +0530

    External new flag css added

--- layouts/header.php
+++ layouts/header.php
@@ -27,7 +27,7 @@ $cached_array = json_decode($file_get_c);
 
 // LOCAL Cache file readingc
 
-$cached_array = json_decode(file_get_contents('http://'.$_SERVER['SERVER_NAME'] . '/demo1/crm/view/b2c_cache.php'));
+$cached_array = json_decode(file_get_contents('http://' . $_SERVER['SERVER_NAME'] . '/demo1/crm/view/b2c_cache.php'));
 
 
 
@@ -59,13 +59,13 @@ for ($i = 0; $i < sizeof($package_tour_data); $i++) {
 
 
 
-  if ($package_tour_data[$i]->tour_type == 'Domestic') {
+    if ($package_tour_data[$i]->tour_type == 'Domestic') {
 
-    array_push($dom_dest_id_arr, intval($package_tour_data[$i]->dest_id));
-  } else if ($package_tour_data[$i]->tour_type == 'International') {
+        array_push($dom_dest_id_arr, intval($package_tour_data[$i]->dest_id));
+    } else if ($package_tour_data[$i]->tour_type == 'International') {
 
-    array_push($int_dest_id_arr, intval($package_tour_data[$i]->dest_id));
-  }
+        array_push($int_dest_id_arr, intval($package_tour_data[$i]->dest_id));
+    }
 }
 
 $dom_dest_id_arr = array_unique($dom_dest_id_arr);
@@ -82,30 +82,30 @@ $dom_array = array();
 
 foreach ($dom_dest_id_arr as $dom_id) {
 
-  foreach ($destination_array as $subarray) {
+    foreach ($destination_array as $subarray) {
 
-    if (isset($subarray->dest_id) && intval($subarray->dest_id) == intval($dom_id)) {
+        if (isset($subarray->dest_id) && intval($subarray->dest_id) == intval($dom_id)) {
 
-      array_push($dom_array, $subarray);
+            array_push($dom_array, $subarray);
 
-      break;
+            break;
+        }
     }
-  }
 }
 
 $intn_array = array();
 
 foreach ($int_dest_id_arr as $int_id) {
 
-  foreach ($destination_array as $subarray) {
+    foreach ($destination_array as $subarray) {
 
-    if (isset($subarray->dest_id) && intval($subarray->dest_id) == intval($int_id)) {
+        if (isset($subarray->dest_id) && intval($subarray->dest_id) == intval($int_id)) {
 
-      array_push($intn_array, $subarray);
+            array_push($intn_array, $subarray);
 
-      break;
+            break;
+        }
     }
-  }
 }
 
 // ////////// Header Holiday domestic and internal destinations End //////////////////
@@ -128,13 +128,13 @@ for ($i = 0; $i < sizeof($group_tour_data); $i++) {
 
 
 
-  if ($group_tour_data[$i]->tour_type == 'Domestic') {
+    if ($group_tour_data[$i]->tour_type == 'Domestic') {
 
-    array_push($dom_dest_id_arr, intval($group_tour_data[$i]->dest_id));
-  } else if ($group_tour_data[$i]->tour_type == 'International') {
+        array_push($dom_dest_id_arr, intval($group_tour_data[$i]->dest_id));
+    } else if ($group_tour_data[$i]->tour_type == 'International') {
 
-    array_push($int_dest_id_arr, intval($group_tour_data[$i]->dest_id));
-  }
+        array_push($int_dest_id_arr, intval($group_tour_data[$i]->dest_id));
+    }
 }
 
 $dom_dest_id_arr = array_unique($dom_dest_id_arr);
@@ -147,30 +147,30 @@ $group_dom_array = array();
 
 foreach ($dom_dest_id_arr as $dom_id) {
 
-  foreach ($destination_array as $subarray) {
+    foreach ($destination_array as $subarray) {
 
-    if (isset($subarray->dest_id) && intval($subarray->dest_id) == intval($dom_id)) {
+        if (isset($subarray->dest_id) && intval($subarray->dest_id) == intval($dom_id)) {
 
-      array_push($group_dom_array, $subarray);
+            array_push($group_dom_array, $subarray);
 
-      break;
+            break;
+        }
     }
-  }
 }
 
 $group_intn_array = array();
 
 foreach ($int_dest_id_arr as $int_id) {
 
-  foreach ($destination_array as $subarray) {
+    foreach ($destination_array as $subarray) {
 
-    if (isset($subarray->dest_id) && intval($subarray->dest_id) == intval($int_id)) {
+        if (isset($subarray->dest_id) && intval($subarray->dest_id) == intval($int_id)) {
 
-      array_push($group_intn_array, $subarray);
+            array_push($group_intn_array, $subarray);
 
-      break;
+            break;
+        }
     }
-  }
 }
 
 // ////////// Header Holiday domestic and internal destinations End //////////////////
@@ -190,8 +190,8 @@ foreach ($int_dest_id_arr as $int_id) {
 
     <!-- Meta Tags -->
     <?php
-    $meta_tags =$cached_array[0]->cms_data[2]->b2c_meta_tags; 
-    
+    $meta_tags = $cached_array[0]->cms_data[2]->b2c_meta_tags;
+
     ?>
     <meta charset="utf-8" />
 
@@ -205,6 +205,8 @@ foreach ($int_dest_id_arr as $int_id) {
 
 
     <!-- Theme Styles -->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@6.6.6/css/flag-icons.min.css" />
 
     <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
 
@@ -448,7 +450,7 @@ foreach ($int_dest_id_arr as $int_id) {
 
                                                     <?php
 
-                          for ($i = 0; $i < sizeof($group_dom_array); $i++) { ?>
+                                                    for ($i = 0; $i < sizeof($group_dom_array); $i++) { ?>
 
                                                     <li><a
                                                             onclick="get_tours_data('<?= $group_dom_array[$i]->dest_id ?>','2')"><?= $group_dom_array[$i]->dest_name ?></a>
@@ -466,7 +468,7 @@ foreach ($int_dest_id_arr as $int_id) {
 
                                                     <?php
 
-                          for ($i = 0; $i < sizeof($group_intn_array); $i++) { ?>
+                                                    for ($i = 0; $i < sizeof($group_intn_array); $i++) { ?>
 
                                                     <li><a
                                                             onclick="get_tours_data('<?= $group_intn_array[$i]->dest_id ?>','2')"><?= $group_intn_array[$i]->dest_name ?></a>
@@ -496,7 +498,7 @@ foreach ($int_dest_id_arr as $int_id) {
 
                                                     <?php
 
-                          for ($i = 0; $i < sizeof($dom_array); $i++) { ?>
+                                                    for ($i = 0; $i < sizeof($dom_array); $i++) { ?>
 
                                                     <li><a
                                                             onclick="get_tours_data('<?= $dom_array[$i]->dest_id ?>','1')"><?= $dom_array[$i]->dest_name ?></a>
@@ -514,7 +516,7 @@ foreach ($int_dest_id_arr as $int_id) {
 
                                                     <?php
 
-                          for ($i = 0; $i < sizeof($intn_array); $i++) { ?>
+                                                    for ($i = 0; $i < sizeof($intn_array); $i++) { ?>
 
                                                     <li><a
                                                             onclick="get_tours_data('<?= $intn_array[$i]->dest_id ?>','1')"><?= $intn_array[$i]->dest_name ?></a>
@@ -617,8 +619,8 @@ foreach ($int_dest_id_arr as $int_id) {
 
     <?php
 
-  // include "get_cache_tax_rules.php";
+    // include "get_cache_tax_rules.php";
 
-  ?>
+    ?>
 
     <input type="hidden" id='cache_currencies' value='<?= $data ?>' />
